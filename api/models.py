c84a6bc313978b972509b1cb49e938ce70bbf9d5
TonyBimenyi <tonyblaisb@gmail.com>
2022-02-10 23:27:50 +0200

    all

--- api/models.py
+++ api/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 
 # Create your models here.
+
+
+class Providers(models.Model):
+    id = models.AutoField
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=100)
+    birthdate = models.DateField
+    phone = models.BigIntegerField 
+
+  
