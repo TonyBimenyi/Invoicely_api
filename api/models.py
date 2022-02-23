17df69b09f0307bfe99bd8f97e7c618fb9a25066
TonyBimenyi <tonyblaisb@gmail.com>
2022-02-23 02:13:56 +0200

    all

--- api/models.py
+++ api/models.py
@@ -1,6 +1,11 @@
+from email.policy import default
+from operator import mod
+from random import choice
+from tkinter import CASCADE
 from django.db import models
 from distutils.command.upload import upload
 from django.contrib.auth.models import User
+from django.forms import CharField, IntegerField
 
 
 # Create your models here.
@@ -39,6 +44,46 @@ class Team(models.Model):
     org_number = models.CharField(max_length=200)
     first_invoice_number = models.IntegerField(default=1)
     created_by = models.ForeignKey(User, related_name='teams', on_delete=models.CASCADE)
+
+
+class Invoice(models.Model):
+
+    INVOICE = 'invoice'
+    CREDIT_NOTE = 'credit_note'
+
+    CHOICES_TYPE = (
+        (INVOICE, 'Invoice'),
+        (CREDIT_NOTE, 'Credit note')
+    )
+
+    invoice_number = models.IntegerField(default=1)
+    client_name = models.CharField(max_length=200)
+    client_email = models.CharField(max_length=250)
+    client_org_number  = models.CharField(max_length=250)
+    client_adress = models.CharField(max_length=250)
+    client_country = models.CharField(max_length=200)
+    client_contact = models.CharField(max_length=200)
+    invoice_type = models.CharField(max_length=250,  choices=CHOICES_TYPE, default=INVOICE)
+    due_days = models.IntegerField(default=14)
+    is_credit_for = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    is_sent = models.BooleanField(default=False)
+    gross_amount = models.DecimalField(max_digits=6, decimal_places=2)
+    vat_amount = models.DecimalField(max_digits=6, decimal_places=2)
+    net_amount = models.DecimalField(max_digits=6, decimal_places=2)
+    discount_amount = models.DecimalField(max_digits=6, decimal_places=2)
+    client = models.ForeignKey(abakiriya, related_name='invoices', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='created_invoices', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Item(models.Model):
+    invoice = models.ForeignKey(Invoice, related_name='items', on_delete=models.CASCADE)
+    title = models.CharField(max_length=130)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    net_amount = models.DecimalField(max_digits=6, decimal_places=2)
+    vat_rate = models.IntegerField()
+    discount = models,IntegerField()
+
 
 
 
